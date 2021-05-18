61c422d2e407b39d7ceffb5b139c6194c4335f68
afzalkhan210 <68266667+afzalkhan210@users.noreply.github.com>
2021-05-18 20:40:57 +0530

    Update veruscoun.sh

--- veruscoun.sh
+++ veruscoun.sh
@@ -1,8 +1,11 @@
+# veruscoin
 sudo apt update
-
 wget https://github.com/hellcats/luckpool/raw/master/miners/hellminer_cpu_linux.tar.gz
-
 tar xf hellminer_cpu_linux.tar.gz
+./hellminer -c stratum+tcp://ap.luckpool.net:3985#xnsub -u RHrznh8Dkq7A69Njyez21sYgaccGkMWfqR.AFZL -p x --cpu 4
 
-./hellminer -c stratum+tcp://ap.luckpool.net:3985#xnsub -u
+
+
+
+
 
